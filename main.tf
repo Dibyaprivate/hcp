9c02b819e474b09b8bff554a16443ff83c3007a8
DibyaJyotiNayak <121348451+Dibyaprivate@users.noreply.github.com>
2025-04-18 21:38:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,14 @@
 provider "aws" {
-region = "ap-south-1"
+  region = "ap-south-1"
 }
 
 resource "aws_instance" "one" {
-count = 1
-ami = "ami-0f1dcc636b69a6438"
-instance_type = "t2.micro"
+  count         = 1
+  ami           = "ami-0f1dcc636b69a6438"
+  instance_type = "t2.micro"
+  
+  tags = {
+    Name = "DemoInstance"
+  }
 }
+
