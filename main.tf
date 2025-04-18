cb7134659fe716a8c3b0f94c62cb7c0e672c23c6
DibyaJyotiNayak <121348451+Dibyaprivate@users.noreply.github.com>
2025-04-18 21:35:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,14 +3,7 @@ region = "ap-south-1"
 }
 
 resource "aws_instance" "one" {
-count = 4
+count = 1
 ami = "ami-0f1dcc636b69a6438"
 instance_type = "t2.micro"
-tags = {
-Name = var.instance_names[count.index]
-}
-}
-
-variable "instance_names" {
-default = ["jenkins", "tomcat-1", "tomcat-2", "Monitoring server"]
 }
