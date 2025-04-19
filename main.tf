c7f25fa1bd8ecc26aabf28bbfcdcaa567c67bca6
DibyaJyotiNayak <121348451+Dibyaprivate@users.noreply.github.com>
2025-04-19 09:22:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,19 @@
 provider "aws" {
-  region = "us-east-1"
+region = "us-east-1"
 }
 
 resource "aws_instance" "one" {
-  count         = 1
-  ami           = "ami-0e449927258d45bc4"
-  instance_type = "t2.micro"
-  
-  tags = {
-    Name = "DemoInstance"
-  }
+count = 4
+ami = "ami-0e449927258d45bc4"
+instance_type = "t2.medium"
+key_name = "virginya"
+vpc_security_group_ids = ["sg-0ae6bccc6af2835c0"]
+tags = {
+Name = var.instance_names[count.index]
+}
+}
+
+variable "instance_names" {
+default = ["jenkins", "tomcat-1", "tomcat-2", "Monitoring server"]
 }
 
