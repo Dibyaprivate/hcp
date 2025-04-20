4d84ef660355206ed024475c606e485ff379044b
DibyaJyotiNayak <121348451+Dibyaprivate@users.noreply.github.com>
2025-04-20 09:53:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "one" {
 count = 5
 ami = "ami-080b1a55a0ad28c02"
 instance_type = "t2.micro"
-key_name = "virginya"
+key_name = "Dibya"
 vpc_security_group_ids = ["sg-03f18dabb38ca5e1e"]
 tags = {
 Name = var.instance_names[count.index]
