1ee86583f836ff8ee7ead974276fbf8c621e32a8
DibyaJyotiNayak <121348451+Dibyaprivate@users.noreply.github.com>
2025-04-20 09:49:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ count = 5
 ami = "ami-080b1a55a0ad28c02"
 instance_type = "t2.micro"
 key_name = "virginya"
-vpc_security_group_ids = ["sg-0ae6bccc6af2835c0"]
+vpc_security_group_ids = ["sg-03f18dabb38ca5e1e"]
 tags = {
 Name = var.instance_names[count.index]
 }
